c233ee8a117c1d2eb230229329ebc01032b0ceaf
pratit8910 <pratitpatel8910@gmail.com>
2025-08-20 20:02:26 +0530

    Add files via upload

---
+++ Q-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+float c,d;
+printf("ENTER FIRST NUMBER:");
+scanf("%f",&c);
+
+printf("ENTER SECOND NUMBER:");
+scanf("%f",&d);
+
+float f=c+d;
+printf("ADDITION IS=%f",f);
+
+
+}
