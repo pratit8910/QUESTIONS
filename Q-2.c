8af7211d7bfbdfcf2a533639d4a40340f7c88b1c
pratit8910 <pratitpatel8910@gmail.com>
2025-08-20 20:10:01 +0530

    Add files via upload

---
+++ Q-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c;
+printf("ENTER FIRST NUMBER:");
+scanf("%f",&a);
+
+printf("ENTER SECOND NUMBER:");
+scanf("%f",&b);
+
+c=a-b;
+printf("SUBTRACTION IS:%f",c);
+
+
+}
